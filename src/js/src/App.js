cb3ac3f2d1835d32cc592798104699888c764d23
barhan44 <gunyashov44@gmail.com>
2020-02-20 23:00:15 +0300

    Add: fetch data from backend on frontend

--- src/js/src/App.js
+++ src/js/src/App.js
@@ -1,25 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { getAllStudents } from './client';
 
 function App() {
+  getAllStudents().then(res => {
+    res.json().then(students => {
+      console.log(students);
+    });
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <h1>Spring Boot + React Project</h1>
   );
 }
 
